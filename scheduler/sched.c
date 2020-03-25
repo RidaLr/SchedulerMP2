5b12e3300ec59c75617f83255245629daf4f9c67
RidaLr <rida.laksir@usmba.ac.ma>
2020-03-25 21:56:27 +0100

    MP2 ( PART I - QST 5) : Implementation of fcfs scheduler.
    I implemented the fcfs function.
    And I have checked that I find the same schedule as in course.

--- scheduler/sched.c
+++ scheduler/sched.c
@@ -42,8 +42,11 @@ tproc * randomscheduler(tlist * procs, tlist * ready, int * delta) {
 
 /* --Scheduler fcfs-- */
 tproc * fcfs(tlist * procs, tlist * ready, int * delta) {
-    /* FIXME: Random scheduler, replace by appropriate code */
-    return randomscheduler(procs, ready, delta);
+    /* FIXED : first come first served */
+    /* Chose the first ready process in ready tlist*/
+    tnode* chosen=ready->first;
+    *delta=chosen->proc->remaining; // how much time is left
+    return chosen->proc;
 }
 /* --Scheduler fcfs-- */
 
