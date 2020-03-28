280b47c4cc5aeb1b380d6ae71c03622a49a36f27
RidaLr <rida.laksir@usmba.ac.ma>
2020-03-28 19:04:56 +0100

    MP2 ( PART III - QST 16) : Extend the scheduling simulator to test the two real-time scheduling algorithms: RM and EDF
    Modify the task_descriptio.h file, so as to simulate a different task system.

--- scheduler/task_description.h
+++ scheduler/task_description.h
@@ -1,8 +1,16 @@
-int max_time = 20;
+/*int max_time = 20;
 tproc tasks[] = {
     {1,0,3,3},
     {2,2,6,6},
     {3,4,4,4},
     {4,6,5,5},
     {5,8,2,2}
+};*/
+
+int max_time = 40;
+tproc tasks[] = {
+     //pid  //activation  //length  //remaining  //period
+    {1,     0,            1,        1,           3       },
+    {2,     0,            1,        1,           4       },
+    {3,     0,            2,        2,           6       },
 };
